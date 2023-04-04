d30a31f3a40f304df548aa1d62f93f993e5aa7ec
singhsitanshu <singhsitanshu2006@gmail.com>
2023-04-03 19:51:57 -0500

    Added code for gallery on main page

--- app.js
+++ app.js
@@ -5,3 +5,23 @@ menu.addEventListener('click', function() {
     menu.classList.toggle('is-active');
     menuLinks.classList.toggle('active');
 });
+
+let scrollContainer = document.querySelector(".gallery");
+    let backBtn = document.getElementById("backBtn");
+    let nextkBtn = document.getElementById("nextBtn");
+
+    scrollContainer.addEventListener("wheel", (evt) => {
+        evt.preventDefault();
+        scrollContainer.scrollLeft += evt.deltaY;
+        scrollContainer.style.scrollBehavior = "auto";
+    })
+
+    nextBtn.addEventListener("click", ()=> {
+        scrollContainer.style.scrollBehavior = "smooth";
+        scrollContainer.scrollLeft += 300;
+    })
+
+    backBtn.addEventListener("click", ()=> {
+        scrollContainer.style.scrollBehavior = "smooth";
+        scrollContainer.scrollLeft -= 300;
+    })
