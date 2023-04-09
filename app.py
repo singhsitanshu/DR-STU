0d6ccc446bebec7424c93328473f329dc2e9503f
singhsitanshu <singhsitanshu2006@gmail.com>
2023-04-09 15:53:45 -0500

    Added CSS styling to details

--- app.py
+++ app.py
@@ -3,7 +3,8 @@ from flask import Flask, render_template
 app = Flask(__name__, static_folder="./static")
 
 products = [
-    {'name': "philly dunks", "price": "35.99", "img":"/images/philly_dunks.png", "desc":"Paying homage to the Philadelphia Phillies, the Nike SB Dunk Low Philadelphia Phillies, boasts a Valor blue Durabuck and suede upper, Team Maroon Swooshes, and woven tongue labels with graphics depicting two Philly staples: the Liberty Bell and a Philly cheese steak sandwich. From there, a white and gum brown Nike Zoom sole completes the design. "}
+    {'name': "Philly Dunks", "price": "35.99", "img":"static/images/philly_dunks.png", "desc":"Paying homage to the Philadelphia Phillies, the Nike SB Dunk Low Philadelphia Phillies, boasts a Valor blue Durabuck and suede upper, Team Maroon Swooshes, and woven tongue labels with graphics depicting two Philly staples: the Liberty Bell and a Philly cheese steak sandwich. From there, a white and gum brown Nike Zoom sole completes the design. "}
+    {'name':'', 'price':'', 'img':'', 'desc':''}
 ]
 
 @app.route('/')
