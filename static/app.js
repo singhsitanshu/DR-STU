334829a00910cec809faaf663a136b16c823e891
singhsitanshu <singhsitanshu2006@gmail.com>
2023-04-09 17:02:43 -0500

    Sent JS to Flask

---
+++ static/app.js
@@ -0,0 +1,31 @@
+const menu = document.querySelector('#mobile-menu');
+const menuLinks = document.querySelector('.navbar__menu');
+
+menu.addEventListener('click', function() {
+    menu.classList.toggle('is-active');
+    menuLinks.classList.toggle('active');
+});
+
+function redirectToNewPage() {
+    window.location.href = "/details-page";
+    }
+
+let scrollContainer = document.querySelector(".gallery");
+    let backBtn = document.getElementById("backBtn");
+    let nextBtn = document.getElementById("nextBtn");
+
+    scrollContainer.addEventListener("wheel", (evt) => {
+        evt.preventDefault();
+        scrollContainer.scrollLeft += evt.deltaY;
+        scrollContainer.style.scrollBehavior = "auto";
+    })
+
+    nextBtn.addEventListener("click", ()=> {
+        scrollContainer.style.scrollBehavior = "smooth";
+        scrollContainer.scrollLeft += 645; // adjust this value as needed
+    });
+    
+    backBtn.addEventListener("click", ()=> {
+        scrollContainer.style.scrollBehavior = "smooth";
+        scrollContainer.scrollLeft -= 645; // adjust this value as needed
+    });    
