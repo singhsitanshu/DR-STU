837428cfa3658b83c1f3f32dec5995e73450d230
singhsitanshu <singhsitanshu2006@gmail.com>
2023-04-09 15:12:28 -0500

    Fixed hierarchy

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 
-app = Flask(__name__, static_folder="./images")
+app = Flask(__name__, static_folder="./static")
 
 products = [
     {'name': "philly dunks", "price": "35.99", "img":"/images/philly_dunks.png", "desc":"Paying homage to the Philadelphia Phillies, the Nike SB Dunk Low Philadelphia Phillies, boasts a Valor blue Durabuck and suede upper, Team Maroon Swooshes, and woven tongue labels with graphics depicting two Philly staples: the Liberty Bell and a Philly cheese steak sandwich. From there, a white and gum brown Nike Zoom sole completes the design. "}
