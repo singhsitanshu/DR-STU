685620f54923f9e0a1b60afc042811ac91385513
singhsitanshu <singhsitanshu2006@gmail.com>
2023-04-09 17:50:27 -0500

    Fixed cards on home page, implementing pages

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, url_for
 
-app = Flask(__name__, static_folder="./static")
+app = Flask(__name__, static_folder="./static", static_url_path='/static')
+
 
 products = [
     {'name': "Philly Dunks", "price": "35.99", "img":"static/images/philly_dunks.png", "desc":"Paying homage to the Philadelphia Phillies, the Nike SB Dunk Low Philadelphia Phillies, boasts a Valor blue Durabuck and suede upper, Team Maroon Swooshes, and woven tongue labels with graphics depicting two Philly staples: the Liberty Bell and a Philly cheese steak sandwich. From there, a white and gum brown Nike Zoom sole completes the design. "}, 
@@ -10,6 +11,18 @@ products = [
 @app.route('/')
 def index():
     return render_template('index.html', css_file=url_for('static', filename='css/styles.css'))
+
+@app.route('/dunks')
+def dunks():
+    return render_template('dunks.html', css_file=url_for('static', filename='css/styles.css'))
+
+@app.route('/jordans')
+def jordans():
+    return render_template('jordans.html', css_file=url_for('static', filename='css/styles.css'))
+
+@app.route('/apparel')
+def apparel():
+    return render_template('apparel.html', css_file=url_for('static', filename='css/styles.css'))
            
 @app.route('/details-page')
 def details_page():
